dc8c590429a7e8a678c866099bf179e0fc1a3290
Simon <simon.fraipont@gmail.com>
2019-01-31 15:15:13 +0100

    légère avancée modale

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ import "./style.scss";
 */
 
 console.log("Hey look in your browser console. It works!");
+
+let addNewIdea = () => {
+  let contentDiv = document.getElementById("idBox");
+  let newHeading = document.createElement("h2");
+  newHeading.setAttribute("id", "ideaName");
+  newHeading.value = document.getElementById("addIdeaName").value;
+  let newParagraph = document.createElement("p");
+  newParagraph.id = "ideaDescription";
+  newParagraph.value = document.getElementById("addIdeaDescription").value;
+  contentDiv.appendChild(newHeading);//computer says no
+  contentDiv.appendChild(newParagraph);//computer says no
+
+  //contentDiv.innerHTML ="<h2>" + newHeading.value + "</h2><p>" + newParagraph.value + "</p>";//fonctionne
+};
+
+document.getElementsByClassName("btn btn-primary")[0].addEventListener('click', () => {
+  addNewIdea();
+});
