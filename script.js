2609b605b75dc28eff70e4be5b5f236a607707c0
matthieu <matthieu.jasselette@gmail.com>
2019-02-04 10:31:23 +0100

    ajout modale de display et bouton cliquable généré dynamiquement

--- script.js
+++ script.js
@@ -16,7 +16,13 @@ let addNewIdea = () => {
   let contentDiv = document.getElementById("ideaBox");
   let ideaDiv = document.createElement("div");
   ideaDiv.className = "ideaElement";
-  let newHeading = document.createElement("h2");
+  let newHeading = document.createElement("button");
+  newHeading.className = "newButton";
+  newHeading.setAttribute("type", "button");
+  newHeading.setAttribute("data-toggle", "modal");
+  newHeading.setAttribute("data-target", "#displayModal");
+  newHeading.setAttribute("name", "display");
+  newHeading.setAttribute("id", "displayButton");
   let newParagraph = document.createElement("p");
   contentDiv.appendChild(ideaDiv);
   ideaDiv.appendChild(newHeading);
@@ -24,6 +30,9 @@ let addNewIdea = () => {
   ideaDiv.appendChild(newParagraph);
   newParagraph.innerText = document.getElementById("addIdeaDescription").value;
 };
+
+// <button class ="newButton" type="button" data-toggle="modal"
+//   data-target="#displayModal" name="display" id ="displayButton"></button>
 
 let ideaStorage = [];
 document.getElementsByClassName("btn btn-primary")[0].addEventListener('click', () => {
