a08271d0c8ef60685d176ffedb3a1e6eea7923e9
matthieu <matthieu.jasselette@gmail.com>
2019-02-04 15:00:30 +0100

    Contenu de la display-modale dynamique

--- script.js
+++ script.js
@@ -12,33 +12,48 @@ import "./style.scss";
   Put the JavaScript code you want below.
 */
 
+//Arrays pour stocker les éléments
+let headingArray = [];
+let paragraphArray = [];
+
+//fonctionne /intégrer les éléments à un array.
 let addNewIdea = () => {
+  let projectNumber = headingArray.length;
+  headingArray.push(document.getElementById("addIdeaName").value);
+  paragraphArray.push(document.getElementById("addIdeaDescription").value)
   let contentDiv = document.getElementById("ideaBox");
   let ideaDiv = document.createElement("div");
-  ideaDiv.className = "ideaElement";
+    ideaDiv.className = "ideaElement";
   let newHeading = document.createElement("button");
-  newHeading.className = "newButton";
-  newHeading.setAttribute("type", "button");
-  newHeading.setAttribute("data-toggle", "modal");
-  newHeading.setAttribute("data-target", "#displayModal");
-  newHeading.setAttribute("name", "display");
-  newHeading.setAttribute("id", "displayButton");
+    newHeading.className = "newButton displayButton";
+    newHeading.setAttribute("type", "button");
+    newHeading.setAttribute("data-toggle", "modal");
+    newHeading.setAttribute("data-target", "#displayModal");
+    newHeading.setAttribute("name", "display");
+    newHeading.addEventListener('click', () => {
+      displayProject(projectNumber);
+    });
   let newParagraph = document.createElement("p");
-  contentDiv.appendChild(ideaDiv);
+    contentDiv.appendChild(ideaDiv);
   ideaDiv.appendChild(newHeading);
-  newHeading.innerText = document.getElementById("addIdeaName").value;
+  newHeading.innerText = headingArray[projectNumber];
   ideaDiv.appendChild(newParagraph);
-  newParagraph.innerText = document.getElementById("addIdeaDescription").value;
+  newParagraph.innerText = paragraphArray[projectNumber];
 };
 
+//fonctionne
+let displayProject = (index) => {
+  document.getElementById("displayName").innerText = headingArray[index] ;
+  document.getElementById("displayDescription").innerText = paragraphArray[index];
+};
+
+//fonctionne
 let addComment = () => {
   let commentDiv = document.getElementById("commentBox");
   let newComment = document.createElement("p");
-  commentDiv.appendChild(newComment)
+  commentDiv.appendChild(newComment);
   newComment.innerText = document.getElementById("commentIdea").value;
 }
-// <button class ="newButton" type="button" data-toggle="modal"
-//   data-target="#displayModal" name="display" id ="displayButton"></button>
 
 let ideaStorage = [];
 
@@ -54,3 +69,4 @@ document.getElementsByClassName("commentButton")[0].addEventListener('click', ()
   addComment();
   document.getElementById("commentIdea").value = '';
 });
+// });
